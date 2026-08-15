1dfa25f0800f66809a42ef4a60632387f45795bb
PayalAnarase <payal.anarase11@gmail.com>
2026-08-15 13:36:57 +0530

    Fix hero image path

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import "./Hero.css";
 import { motion } from "framer-motion"
 function Hero({profile}){
     return(
-        <motion.section className="hero" id="home" id="home" initial={{ opacity: 0, y:80 }} animate={{ opacity:1, y:0}} transition={{ duration: 0.8 }}>
+        <motion.section className="hero" id="home"  initial={{ opacity: 0, y:80 }} animate={{ opacity:1, y:0}} transition={{ duration: 0.8 }}>
             <motion.div className="hero-content" initial = {{ opacity:0,x:-80}} animate={{opacity:1,x:0 }} transition={{ delay:0.3, duration:0.8}}>
                 <p className="greeting">Hi, I'm</p>
                 <h1>Payal Anarase</h1>
@@ -19,10 +19,13 @@ function Hero({profile}){
 
             </motion.div>
 
-            < motion.div className="hero-image"  initial={{ opacity:0,x:80}} animate={{ opacity:1,x:0}} transition={{ delay:0.5, duration:0.8 }}>
-                <img src="/src/assets/images/photo.jpeg" alt="Payal" />
-
-
+            <motion.div
+                className="hero-image"
+                initial={{ opacity: 0, x: 80 }}
+                 animate={{ opacity: 1, x: 0 }}
+                 transition={{ delay: 0.5, duration: 0.8 }}
+            >
+             <img src={photo} alt="Payal" />
             </motion.div>
         </motion.section>
     );
